56b95ff5beaa91f76e0b6fcd78f796269a1e47de
Fatih Demir <f.demir@avisi.nl>
2021-01-14 15:25:38 +0100

    wip

--- src/avisi_apps/server/main.clj
+++ src/avisi_apps/server/main.clj
@@ -47,6 +47,13 @@
 
 (defonce config-storage (atom {}))
 
+(comment
+  @subscriptions
+
+  @config-storage
+
+  )
+
 (defn store-subscription [{:keys [subscriptionId recipeId webhookUrl inputFields project-id webhook-id]}]
   (let [gitlab-project (get-in inputFields [:gitlabProject :value])]
     (swap! subscriptions assoc project-id {:webhook-url webhookUrl
@@ -132,9 +139,9 @@
 (defn handle-gitlab-projects [{:keys [payload user-id] :as env}]
   (let [{:keys [inputFields]} payload
         projects (-> (gitlab-query [(list {:projects
-                                            [{:nodes [:fullPath]}]}
-                                       {:membership true})]
-                        (fetch-gitlab-access-token env))
+                                           [{:nodes [:fullPath]}]}
+                                      {:membership true})]
+                       (fetch-gitlab-access-token env))
                    (get-in [:body :data :projects :nodes]))]
     {:status 200
      :headers {"content-type" "application/json"}
@@ -143,6 +150,20 @@
                                         {:id fullPath
                                          :value fullPath})
                                   projects))}))
+
+(defn monday-board-columns [{:keys [board-id] :as env}]
+  (-> (monday-query [(list {:boards [:id :name
+                                     {:columns [:id
+                                                :title
+                                                :type]}]}
+                   {:ids [board-id]})]
+    (fetch-monday-access-token env))
+    (get-in [:body :data :boards 0 :columns])))
+
+(defn monday-board-text-columns [env]
+  (filter
+    #(contains? #{"text" "long-text" "name"} (:type %))
+    (monday-board-columns env)))
 
 (defn handle-gitlab-create-issue [{:keys [payload user-id] :as env}]
   (let [{:keys [inputFields]} payload
@@ -224,22 +245,26 @@
                :body
                (json/read-value (json/object-mapper {:decode-key-fn true})))]
     {:project-id (:project_id body)
-     :webhook-id (:webhook-id body)}))
+     :webhook-id (:id body)}))
 
-(defn gitlab-remove-webhook [{:keys [payload webhook-id] :as env}]
-  (let [project (-> (get-in payload [:inputFields :gitlabProject :value])
-                  (http-util/url-encode))]
-    (http/delete (str gitlab-api-url "/projects/" project "/hooks/" webhook-id)
-      {:oauth-token (fetch-gitlab-access-token env)})))
+(defn gitlab-remove-webhook [{:keys [payload project-id webhook-id] :as env}]
+  (http/delete (str gitlab-api-url "/projects/" project-id "/hooks/" webhook-id)
+    {:oauth-token (fetch-gitlab-access-token env)}))
 
 (defn handle-gitlab-issue-created-webhook [{:keys [body] :as env}]
-  (let [project-id (get-in body [:project :id])
-        issue-id (get-in body [:object_attributes :id])
-        {:keys [webhook-url]} (fetch-subscription project-id)]
+  (let [project (:project body)
+        issue (:object_attributes body)
+        user (:user body)
+        issue-id (:id issue)
+        {:keys [webhook-url]} (fetch-subscription (:id project))]
     (http/post webhook-url
       {:headers {"authorization" (:monday/signing-secret app-config)}
        :content-type :json
-       :form-params {"trigger" {"outputFields" {"gitLabIssueId" (gitlab-gid issue-id)}}}})
+       :form-params {"trigger" {"outputFields" {"gitlabIssueId" (gitlab-gid issue-id)
+                                                "gitlabIssue" {"title" (:title issue)
+                                                               "description" (:description issue)
+                                                               "authorName" (:name user)
+                                                               "authorUsername" (:username user)}}}}})
     {:status 204}))
 
 (defn monday-item-by-id [{:keys [item-id] :as env}]
@@ -255,35 +280,70 @@
         (fetch-gitlab-access-token env))
     (get-in [:body :data :issue])))
 
-(defn create-monday-item [{:keys [gitlab-issue-id board-id] :as env}]
+(defn create-monday-item [{:keys [gitlab-issue-id board-id column-mapping] :as env}]
   (let [{:keys [title]} (gitlab-issue-by-id env)
         item-id (-> (monday-query
                       [{(list 'create_item
                           {:board_id board-id
-                           :item_name title})
+                           :item_name title
+                           :column_values (json/write-value-as-string column-mapping)})
                         [:id]}]
                       (fetch-monday-access-token env))
                   (get-in [:body :data :create_item :id]))]
     (store-config [:create-n-sync gitlab-issue-id] {:item-id (parse-int item-id)
                                                     :gitlab-issue-id gitlab-issue-id})))
 
-(defn update-monday-item [{:keys [board-id item-id new-columns] :as env}]
+(defn update-monday-item [{:keys [board-id item-id column-mapping] :as env}]
   (monday-query
     [{(list 'change_multiple_column_values
         {:item_id item-id
          :board_id board-id
-         :column_values (json/write-value-as-string new-columns)})
+         :column_values (json/write-value-as-string column-mapping)})
       [:id]}]
     (fetch-monday-access-token env)))
 
-(defn sync-monday-item-with-gitlab-issue [{:keys [board-id item-id new-columns] :as env}]
-  (let [{:keys [title]} (gitlab-issue-by-id env)]
-    (update-monday-item (assoc env :new-columns {:name title}))))
+(defn sync-monday-item-with-gitlab-issue [env]
+  (update-monday-item env))
+
+(defn handle-column-mapping [{:keys [payload] :as env}]
+  (let [{:keys [inputFields]} payload
+        env (assoc env :board-id (:boardId payload))
+        columns (monday-board-text-columns env)
+        res (mapv (fn [{:keys [id title]}]
+                    {:id id
+                     :title title
+                     :outboundType "text"
+                     :inboundTypes ["text"]})
+              columns)]
+    {:status 200
+     :headers {"content-type" "application/json"}
+     :body
+     (json/write-value-as-bytes res)}))
+
+(defn handle-gitlab-issue [env]
+  {:status 200
+   :headers {"content-type" "application/json"}
+   :body
+   (json/write-value-as-bytes (mapv
+                                (fn [{:keys [id title]}]
+                                  {:id id
+                                   :title title
+                                   :outboundType "text"
+                                   :inboundTypes ["text"]})
+                                [{:id "title"
+                                  :title "Title"}
+                                 {:id "description"
+                                  :title "Description"}
+                                 {:id "authorName"
+                                  :title "Author Name"}
+                                 {:id "authorUsername"
+                                  :title "Author Username"}]))})
 
 (defn handle-create-n-sync-item [{:keys [payload] :as env}]
-  (let [{:keys [gitLabIssueId boardId]} (:inputFields payload)
-        item-id (fetch-config [:create-n-sync gitLabIssueId :item-id])
-        env (assoc env :gitlab-issue-id gitLabIssueId
+  (let [{:keys [gitlabIssueId boardId columnMapping]} (:inputFields payload)
+        item-id (fetch-config [:create-n-sync gitlabIssueId :item-id])
+        env (assoc env :gitlab-issue-id gitlabIssueId
+                       :column-mapping columnMapping
                        :board-id boardId
                        :item-id item-id)]
     (cond
@@ -300,13 +360,27 @@
       [["/integrations"
         {:middleware [[monday-authentication-middleware]]}
         ["/field-types"
+         ["/gitlab-issue"
+          {:post {:parameters {:body [:map]}
+                  :responses {200 {:body [any?]}}
+                  :handler (fn [{{{:keys [payload]} :body} :parameters :as request}]
+                             (log/info "gitlab-issue")
+                             (handle-gitlab-issue {:payload payload
+                                                   :user-id (get-in request [:jwt :userId])}))}}]
          ["/gitlab-projects"
           {:post {:parameters {:body [:map]}
                   :responses {200 {:body [any?]}}
                   :handler (fn [{{{:keys [payload]} :body} :parameters :as request}]
                              (log/info "gitlab-projects")
                              (handle-gitlab-projects {:payload payload
-                                                      :user-id (get-in request [:jwt :userId])}))}}]]
+                                                      :user-id (get-in request [:jwt :userId])}))}}]
+         ["/column-mapping"
+          {:post {:parameters {:body [:map]}
+                  :responses {200 {:body [any?]}}
+                  :handler (fn [{{{:keys [payload]} :body} :parameters :as request}]
+                             (log/info "column mapping")
+                             (handle-column-mapping {:payload payload
+                                                     :user-id (get-in request [:jwt :userId])}))}}]]
         ["/action"
          ["/gitlab"
           ["/create-issue"
@@ -321,7 +395,6 @@
                    :responses {200 {:body [any?]}}
                    :handler (fn [{{{:keys [payload]} :body} :parameters :as request}]
                               (log/info "create-n-sync-item")
-
                               (handle-create-n-sync-item {:payload payload
                                                           :user-id (get-in request [:jwt :userId])}))}}]]]
         ["/subscribe"
@@ -344,7 +417,9 @@
                                                      webhook-id (:webhook-id (fetch-subscription project-id))]
                                                  (remove-subscription project-id)
                                                  (gitlab-remove-webhook {:payload payload
-                                                                         :webhook-id webhook-id})
+                                                                         :project-id project-id
+                                                                         :webhook-id webhook-id
+                                                                         :user-id (get-in request [:jwt :userId])})
                                                  {:status 204}))}}]]]]
        ["/webhooks"
         ["/gitlab"
@@ -364,9 +439,7 @@
                             (redirect-response (cond
                                                  (and gitlab-authorized? monday-authorized?) (state->back-to-url token)
                                                  monday-authorized? (gitlab-authorize-url token)
-                                                 :else (monday-authorize-url token)))
-                            #_{:status 200
-                             :body "<html>\n       <meta charset=\"utf-8\">\n       <body>\n       <div id=\"app\"></div>\n       <script src=\"/js/board-view.js\"></script>\n       </body>\n       </html>"}))}}]
+                                                 :else (monday-authorize-url token)))))}}]
 
        ["/oauth/callback"
         {:get {:handler (fn [request]
@@ -417,6 +490,15 @@
 
   ;; user-id
   ;; 16227277
+
+  (monday-query [(list {:boards [:id :name
+                                 {:columns [:id
+                                            :title]}
+                                #_(list {:column_values [:id]}
+                                  {:ids "text"})]}
+                   {:ids [749796977]})]
+    (get-in @oauth-tokens [:monday 16227277 :access_token]))
+
 
   (monday-query [(list {:items [:id :name
                                 (list {:column_values [:id]}
@@ -493,6 +575,34 @@
       {:oauth-token (get-in @oauth-tokens [:gitlab 16227277 :access_token])}))
 
   (http-util/url-encode "fatihict/fatih-test-private-repo")
+
+  projects (-> (gitlab-query [(list {:projects
+                                     [{:nodes [:fullPath]}]}
+                                {:membership true})]
+                 (fetch-gitlab-access-token env))
+             (get-in [:body :data :projects :nodes]))
+
+  (monday-query [(list {:boards [:id :name
+                                 {:columns [:id
+                                            :title
+                                            :type]}]}
+                   {:ids [(:board-id dev-env)]})]
+    (fetch-monday-access-token dev-env))
+
+
+
+  (let [column-mapping {:long_text "ooh oke, now I get it",
+                        :name "bar",
+                        :text2 "fdemir",
+                        :text "Fatih"}
+        board-id (:board-id dev-env)]
+    (monday-query
+      [{(list 'create_item
+          (cond-> {:board_id board-id
+                   :item_name (:name column-mapping)}
+            (seq columns) (assoc :column_values (json/write-value-as-string column-mapping))))
+        [:id]}]
+      (fetch-monday-access-token dev-env)))
 
   )
 
